7419872be1b730a06e7764f5c2f26e21f210f2a7
developer <luis.castellanos.oviedo@gmail.com>
2019-05-13 10:39:05 -0500

    maven project

---
+++ rest-microservices-artifact/src/main/java/com/rest/services/restmicroservicesartifact/RestMicroservicesArtifactApplication.java
@@ -0,0 +1,13 @@
+package com.rest.services.restmicroservicesartifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestMicroservicesArtifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestMicroservicesArtifactApplication.class, args);
+	}
+
+}
