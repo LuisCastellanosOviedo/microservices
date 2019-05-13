88bd8b967ab536b103d05932c10716c80c320f6d
developer <luis.castellanos.oviedo@gmail.com>
2019-05-13 10:45:16 -0500

    gradle project

---
+++ gradleVersion/rest-service-artifact/src/main/java/com/rest/services/restserviceartifact/RestServiceArtifactApplication.java
@@ -0,0 +1,13 @@
+package com.rest.services.restserviceartifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceArtifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceArtifactApplication.class, args);
+	}
+
+}
