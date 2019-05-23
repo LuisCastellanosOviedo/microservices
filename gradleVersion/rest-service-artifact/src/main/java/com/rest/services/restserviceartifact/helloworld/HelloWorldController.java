cb8d8dd757502749f49bd8e36e60e707b3e057ec
developer <luis.castellanos.oviedo@gmail.com>
2019-05-23 11:49:27 -0500

    hello world bean

--- gradleVersion/rest-service-artifact/src/main/java/com/rest/services/restserviceartifact/helloworld/HelloWorldController.java
+++ gradleVersion/rest-service-artifact/src/main/java/com/rest/services/restserviceartifact/helloworld/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.rest.services.restserviceartifact.helloworld;
 
+import com.rest.services.restserviceartifact.helloworld.domain.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-//http://localhost:8080/hello-world-gradle
+    //http://localhost:8080/hello-world-gradle
     @RequestMapping(method = RequestMethod.GET,path = "/hello-world-gradle")
     public String helloWorldGradle(){
         return "hello world Gradle";
@@ -18,5 +19,12 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-get-mapping-gradle")
     public String helloWorldGetMappingGradle(){
         return "hello world grale get mapping";
+    }
+
+
+    //http://localhost:8080/hello-world-gradle-bean
+    @GetMapping(path = "/hello-world-gradle-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("hello world bean");
     }
 }
--- rest-microservices-artifact/src/main/java/com/rest/services/restmicroservicesartifact/hellorestcontroller/HelloWorldController.java
+++ rest-microservices-artifact/src/main/java/com/rest/services/restmicroservicesartifact/hellorestcontroller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.rest.services.restmicroservicesartifact.hellorestcontroller;
 
+import com.rest.services.restmicroservicesartifact.hellorestcontroller.domain.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,5 +19,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-get-mapping-maven")
     public String helloWorldGetMapping(){
         return "hello world get mapping-maven";
+    }
+
+    //http://localhost:8080/hello-world-bean
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("hello world bean ");
     }
 }
