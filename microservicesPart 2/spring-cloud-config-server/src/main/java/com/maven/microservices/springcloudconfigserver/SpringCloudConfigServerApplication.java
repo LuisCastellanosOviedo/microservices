54d8930dba41560531c7e699c33f379357705fa7
developer <luis.castellanos.oviedo@gmail.com>
2019-05-31 12:35:15 -0500

    add git repo

--- microservicesPart 2/spring-cloud-config-server/src/main/java/com/maven/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ microservicesPart 2/spring-cloud-config-server/src/main/java/com/maven/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,8 +2,13 @@ package com.maven.microservices.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+//url --> http://localhost:8888/limits-service/default
+//http://localhost:8888/limits-service/dev
+//http://localhost:8888/limits-service/qa
 @SpringBootApplication
+@EnableConfigServer
 public class SpringCloudConfigServerApplication {
 
 	public static void main(String[] args) {
